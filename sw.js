341f42b4510b7871181b82ad761eca048d8a3d84
WordZen-Cloud <gameovercr7200@gmail.com>
2026-01-08 23:55:41 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-const cacheName = 'wordzen-v100';
+const cacheName = 'wordzen-arena-v1';
 const assets = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon.png' // İkonu da önbelleğe ekledik ki internetsiz görünsün
 ];
 
+// Dosyaları Önbelleğe Al
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
@@ -13,10 +15,28 @@ self.addEventListener('install', e => {
   );
 });
 
+// Eski Önbellekleri Temizle (Güncelleme yapınca eskisini siler)
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// İnternet Kontrollü Fetch (Arena modu için önemli)
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(res => {
-      return res || fetch(e.request);
+      return res || fetch(e.request).catch(() => {
+        // Eğer bir dosya bulunamazsa ve internet yoksa ana sayfayı döndür
+        if (e.request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+      });
     })
   );
 });
